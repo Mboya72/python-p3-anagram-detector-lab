b6420aeeda6168205b8c9314441f7dd67b845d6f
Mboya72 <elvindio72@gmail.com>
2024-11-28 22:40:28 +0300

    pass anagram.py test

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,22 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word_letters = sorted([letter for letter in word])
+
+    def match(self, word_list):
+        match_word_list = []
+
+        for word in word_list:
+            if sorted([letter for letter in word]) == self.word_letters:
+                match_word_list.append(word)
+
+        return match_word_list
+
+
+anagram_checker = Anagram("listen")
+
+word_list = ["enlists", "google", "inlets", "banana"]
+
+result = anagram_checker.match(word_list)
+
+print(result)
---
+++ lib/anagram.test.py
@@ -0,0 +1,4 @@
+from anagram import Anagram
+
+class TestAnagram:
+    pass
